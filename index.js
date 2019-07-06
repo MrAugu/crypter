cf46bf116ff82673071207c943013e898bc61a3e
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-07-06 13:11:14 +0300

    Adding hash and validate functions to index for better look

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ module.exports = {
   hasher: require("./Functions/hash"),
   validator: require("./Functions/validator"),
   author: "MrAugu#9016",
-  version: require("./package.json").version
+  version: require("./package.json").version,
+  hash: require("./Functions/hash").hash,
+  validate: require("./Functions/validator").validate
 };
